4d4e18551b2655df7a04a555c5c457c130484802
noble20048 <noblebijukattungal@gmail.com>
2026-06-19 00:30:14 +0530

    well,tweaked more and now it looks like this,made changes to the display and casing of the calculator,and introduced hovering over buttons

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const display = document.getElementById('display');
+//CALCULATOR PGM
+const display = document.getElementById("display");
 
 function appendToDisplay(input) {
     display.value += input;
 }
 
-function clearDisplay() {
+function clearScreen() {
     display.value = "";
 }
-function deleteLast() {
-    display.value = display.value.slice(0, -1)
-}
+
 function calculate() {
     try {
-        // eval() takes the string "2+2" and turns it into the number 4
         display.value = eval(display.value);
-    } catch (e) {
-        display.value = "Error";
+    }
+    catch (error) {
+        display.value = "error";
     }
 }
